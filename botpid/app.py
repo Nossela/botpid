436d8a136b58214362695d131c0a05647db11eb9
Nossela <alessonlevi200@gmail.com>
2023-09-06 16:30:08 -0300

    subi tudo

---
+++ botpid/app.py
@@ -0,0 +1,33 @@
+from os import getpid
+
+if __name__ == '__main__':
+    
+    try:
+        
+        def log_pid(**kwargs) -> None:
+            
+            with open(kwargs["log_file"], "w") as file:
+                file.write(f"get-Process -ID {kwargs['pid']}\n")
+                file.write(f"Stop-Process -ID {kwargs['pid']} -Force\n")
+                 
+        log_pid(log_file="log_file", pid=getpid())
+    
+    except Exception as e:
+        print(e) 
+    
+    
+   
+    
+            
+        def animais(txt):
+           
+            animais('Elefante')
+            
+            print(animais)
+            
+                
+             
+                
+               
+             
+    
